f4e168aec0370d90f304bb6ca43e6abe9aae72bf
aryan <64030782+aryan-cs@users.noreply.github.com>
2022-11-18 20:18:27 +0000

    reworked sizing & colors

--- src/components/Background.js
+++ src/components/Background.js
@@ -7,33 +7,26 @@ function sketch (p5) {
 	let MOUSING = false;
 	let ELASTICITY_ISH = 0.01;
 	let GRAVITY = 0.01;
-	let ELECTRON_MODE = false;
+	let BACKGROUND_COLOR = "#121212";
+	let PRESET_COLORS = [
 
-	let bodies = [];
-	let mouseVector = null;
-	let presetPurples = [
-
-		"#732e8f",
-  		"#722391",
-  		"#870096",
-  		"#aa30b8",
-  		"#700a7d",
-  		"#9d51a6"
+		"#cc9c00",
+  		"#b08b13",
+  		"#d69600",
+		"#dbb50b"
 
 	];
 
-	if (ELECTRON_MODE) { ELASTICITY_ISH = 0.01; GRAVITY = 0.01; }
-
+	let bodies = [];
+	let mouseVector = null;
+	
 	function Body (x, y, z) {
     
 		this.pos = p5.createVector(x, y, z);
-		this.vel =  p5.createVector(0, 0, 0);
-	
-		if (ELECTRON_MODE) { this.scale =  p5.random(1.75, 2); } else { this.scale =  p5.random(2, 6); }
-	
-		this.speed = 150 * (2 / this.scale);
+		this.vel =  p5.createVector(0, 0, 0);	
+		this.speed = 50;
 		this.angularVel =  p5.random(0.005, 0.025);
-		this.color = presetPurples[p5.floor(p5.random(0, presetPurples.length))];
+		this.color = PRESET_COLORS[p5.floor(p5.random(0, PRESET_COLORS.length))];
 	
 		mouseVector =  p5.createVector(window.innerWidth / 2, window.innerHeight / 2, UNIVERSAL_Z);
 	
@@ -49,8 +42,8 @@ function sketch (p5) {
 	
 		else {
 			
-			mouseVector =  p5.createVector(p5.sin(p5.frameCount / 50) * (window.innerWidth / 4),
-										   p5.cos(p5.frameCount / 50) * (window.innerHeight / 4),
+			mouseVector =  p5.createVector(p5.sin(p5.frameCount / 40) * (Math.min(window.innerHeight, window.innerWidth) / 5),
+										   p5.cos(p5.frameCount / 40) * (Math.min(window.innerHeight, window.innerWidth) / 5),
 										   UNIVERSAL_Z);
 		
 		}
@@ -60,23 +53,24 @@ function sketch (p5) {
 		mouseVector.mult(this.speed);
 		this.vel.lerp(mouseVector, GRAVITY);
 	
-		var seperation =  p5.createVector(0, 0, 0);
-		var seperationCount = 0;
+		let seperation =  p5.createVector(0, 0, 0);
+		let seperationCount = 0;
 	
 		bodies.forEach(body => {
 	
 			if (body !== this) {
 
-				var distance = p5.dist(this.pos, body.pos);
+				let distance = p5.dist(this.pos.x, this.pos.y, body.pos.x, body.pos.y);
+					
+				if (distance < 50) {
 	
-				if (distance < 20 * this.scale) {
-	
-					var difference = p5.Vector.sub(this.pos, body.pos);
+					let difference =  p5.createVector(this.pos.x - body.pos.x, this.pos.y - body.pos.y, 0);
 	
 					difference.normalize();
-					difference.div(distance);
 					seperation.add(difference);
+					seperation.mult(99);
 					seperationCount++;
+					console.log(seperation);
 	
 				}
 	
@@ -99,7 +93,7 @@ function sketch (p5) {
 		p5.translate(this.pos.x, this.pos.y, this.pos.z);
 		p5.rotateX(p5.frameCount * this.angularVel);
 		p5.rotateY(p5.frameCount * this.angularVel);
-		p5.scale(this.scale);
+		p5.scale(2);
 		p5.noFill();
 		p5.stroke(this.color);
 		p5.strokeWeight(2);
@@ -131,7 +125,6 @@ function sketch (p5) {
 		
 		}
 
-		// for some reason, two canvases are created on certain devices
 		if (document.getElementsByTagName("canvas").length > 1) { document.getElementsByTagName("canvas")[0].remove(); }
 
 		p5.resizeCanvas(window.innerWidth, window.innerHeight);
@@ -141,7 +134,7 @@ function sketch (p5) {
 
 	p5.draw = () => {
 
-	  	p5.background("black");
+	  	p5.background(BACKGROUND_COLOR);
   		bodies.forEach(body => { body.update(); body.render(); });
 		
 	};
@@ -153,7 +146,7 @@ function sketch (p5) {
 			let pushy = p5.createVector(p5.mouseX - (window.innerWidth / 2), p5.mouseY - (window.innerHeight / 2), UNIVERSAL_Z);
 			pushy.sub(body.pos);
 			pushy.normalize();
-			pushy.mult(Math.min(window.innerHeight, window.innerWidth) / -35);
+			pushy.mult(Math.min(window.innerHeight, window.innerWidth) / -30);
 			body.vel.add(pushy);
 			
 		});
