f8179065e81a99a1ef9b3d7e2b3c01f14ce1180c
aryan-cs <aryan05g@gmail.com>
2023-06-02 13:37:42 -0400

    site revamp! old one was really laggy and mid

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# aryan's website
+# aryan's website v2
 
 this is a work in progress!
--- src/index.js
+++ src/index.js
@@ -5,13 +5,4 @@ import App from './App';
 import Background from "./components/Background";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
-  <React.StrictMode>
-
-    <Background />
-    <App />
-    
-  </React.StrictMode>
-
-);
+root.render(<React.StrictMode><App/></React.StrictMode>);
