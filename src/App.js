90c7c4f2374d06070e4e90c59f834f70d1dde74b
aryan-cs <aryan05g@gmail.com>
2023-06-04 21:22:17 -0500

    silver!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Link, animateScroll as scroll } from "react-scroll";
+import React from "react";
+// import { Link, animateScroll as scroll } from "react-scroll";
 import "./App.css";
 
 import Landing from "./pages/Landing";
@@ -7,11 +7,18 @@ import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
+import background from "./assets/videos/background.mp4";
+// import background from "https://player.vimeo.com/video/804348447?background=1";
+
 function App () {
 
        return (
 
               <div className = "App">
+
+                     <video autoPlay loop muted className = "bg-vid">
+                            <source src = {background} />
+                     </video>
 
                      <Landing />
                      <About />
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -6,7 +6,13 @@ function About () {
 
         <div className = "Landing" id = "landing">
 
+            <h1 className = "symbol"> Ag </h1>
+            <h2 className = "title"> ARYAN GUPTA </h2>
+            <p> Creating a better world through technology. </p>
 
+            <div className = "logo-container">
+                <img src = {require("../assets/images/icon.png")} alt = "uh oh" />
+            </div>
 
         </div>
 
